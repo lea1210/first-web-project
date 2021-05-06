3fb48782903a08c0427f666342d55233ad096f3b
Lea Rörig <lea.roerig@student.hs-rm.de>
2021-05-06 18:14:16 +0200

    Internationalisierung angepasst.

--- src/main/java/de/hsrm/mi/web/projekt/internationalization/InternationalConfiguration.java
+++ src/main/java/de/hsrm/mi/web/projekt/internationalization/InternationalConfiguration.java
@@ -3,8 +3,10 @@ package de.hsrm.mi.web.projekt.internationalization;
 import java.util.Locale;
 
 import org.hibernate.validator.spi.messageinterpolation.LocaleResolver;
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
@@ -25,6 +27,15 @@ public class InternationalConfiguration implements WebMvcConfigurer{
         LocaleChangeInterceptor interceptor =new LocaleChangeInterceptor();
         interceptor.setParamName("sprache");
         return interceptor;
+    }
+
+    @Bean
+    public MessageSource messageSource(){
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasename("classpath:messages");
+
+        return messageSource;
+
     }
     
     @Override
