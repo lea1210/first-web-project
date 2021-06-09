46a16185bb687b0988059453c09a3a6e633da76f
Lea Rörig <lea.roerig@student.hs-rm.de>
2021-06-09 16:25:00 +0200

    Einfuehrung frontend

---
+++ frontend/src/components/FotoGalerie.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="container">
+    <!-- Button zum Hinzufügen des nächsten Bildes -->
+    <button class="button">
+      <i class="fas fa-camera" />
+    </button>
+    <!-- Eingabefeld für inkrementelle Suche -->
+    <section class="section">
+      <input type="text" class="input" placeholder="Suche" />
+    </section>
+    <section class="section">
+      <div class="columns is-multiline">
+        <!-- Hier alle Bilder mit Hilfe der FotoGalerieBild-Komponente anzeigen -->
+        <!-- flexibel natürlich - nicht die fünf Beispielbilder hardcoden! -->
+      </div>
+    </section>
+  </div>
+</template>
+
+<script lang="ts">
+import { computed, defineComponent, ref, Ref } from "vue";
+import FotoGalerieBild from "./FotoGalerieBild.vue";
+import { Foto } from "@/services/Foto";
+import { fotoliste } from "@/services/FotoListe";
+
+export default defineComponent({
+  name: "FotoGalerie",
+
+  
+});
+</script>
+
+
+<style scoped>
+
+</style>
---
+++ frontend/src/components/FotoGalerieBild.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="card column is-3 has-background-grey-lighter m-3">
+    <div class="card-header">
+      <p class="card-header-title is-centered">
+        <!-- Dateinamen ausgeben -->
+      </p>
+      <!-- Lösch-Button -->
+      <button class="button card-header-icon has-background-grey-light">
+        <i class="fa fa-times" />
+      </button>
+    </div>
+    <div class="card-content has-text-centered">
+      <!-- Bild anzeigen -->
+      <figure class="image is-inline-block">
+        <img :src="url" />
+      </figure>
+      <div class="content">
+        <foto-star-rating :maxsterne="5" />
+      </div>
+      <!-- Ort -->
+      <div class="content">Irgendwo</div>
+      <!-- Zeitstempel -->
+      <div class="has-text-grey">Irgendwann</div>
+    </div>
+  </div>
+</template>
+
+
+<script lang="ts">
+import { defineComponent, PropType, ref } from "vue";
+import FotoStarRating from "./FotoStarRating.vue";
+import { Foto } from "@/services/Foto";
+
+export default defineComponent({
+  components: { FotoStarRating },
+  name: "FotoGalerieBild",
+  setup(props, context) {
+
+    
+    return {
+      url: require("@/assets/thumbnails/DerTupel.png")
+    };
+  }
+});
+</script>
